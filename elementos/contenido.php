af9ca43b4925c27fe97d34eefa93afa51bc0bbab
JuanJoseFernandez10 <fernandez.suarez.juan.jose@iescamas.es>
2025-10-06 10:09:03 +0200

    Reemplazado bucle for por foreach

--- elementos/contenido.php
+++ elementos/contenido.php
@@ -1,12 +1,3 @@
-<?php
-// Estructura asociativa: producto => precio
-$productos = [
-  "Pan de Camas"        => 1.20,
-  "Aceitunas aliñadas"  => 2.50,
-  "Tortas de aceite"    => 3.00
-];
-?>
-
 <h2 class="text-success text-center mt-4">Productos locales de Camas</h2>
 
 <table class="table table-bordered table-striped w-75 mx-auto mt-4 text-center align-middle">
@@ -17,11 +8,21 @@ $productos = [
     </tr>
   </thead>
   <tbody>
-    <?php foreach ($productos as $nombre => $precio): ?>
-      <tr>
-        <td><?= htmlspecialchars($nombre) ?></td>
-        <td><?= number_format($precio, 2, ',', '.') ?></td>
-      </tr>
-    <?php endforeach; ?>
+    <?php 
+      // Estructura asociativa: producto => precio
+      $productos = [
+      "Pan de Camas"        => 1.20,
+      "Aceitunas aliñadas"  => 2.50,
+      "Tortas de aceite"    => 3.00
+      ];
+      $string_keys = array_keys($productos);
+      $i = 0;
+      while($i < count($productos)){
+        echo "<tr>";
+        echo "<td>" . $string_keys[$i] . "</td> <td>" . $productos[$string_keys[$i]] . "</td>";
+        echo "</tr>";
+        $i++;
+      }
+    ?>
   </tbody>
 </table>
